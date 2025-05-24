92d14b1708982842c0f500d492cebc4867a3d474
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-05-24 13:16:44 +0300

    add <ul className={css.grid}>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,34 @@
 // import css from "./App.css";
 import SearchBar from "../SearchBar/SearchBar";
 import movieService from "../../services/movieService";
+import { useState } from "react";
+import type { Movie } from "../../types/movie";
+import toast from "react-hot-toast";
 
 export default function App() {
-  const handleSubmit = (movie: string) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const handleSubmit = async (movie: string) => {
     try {
-      const res = movieService();
-    } catch (error) {
+      setIsLoading(true);
+      setIsError(false);
+      const fetchMovies = await movieService(movie);
+      if (fetchMovies.length === 0) {
+        toast.error("No movies found for your request.");
+      }
+      setMovies(fetchMovies);
+    } catch {
+      setIsError(true);
     } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <>
       <SearchBar onSubmit={handleSubmit} />
+      {/* {isLoading && } */}
     </>
   );
 }
