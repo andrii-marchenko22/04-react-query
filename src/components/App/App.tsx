7371e97413dc0dc91ce3c5202b38c1ecc5d7ab0e
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-06-02 01:11:52 +0300

    add ReactPaginate

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import css from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
 import movieService from "../../services/movieService";
 import { useState, useEffect } from "react";
@@ -9,6 +10,7 @@ import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
 
 export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -17,14 +19,12 @@ export default function App() {
   const [query, setQuery] = useState("");
   const [currentPage, setCuurentPage] = useState(1);
 
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError, isSuccess } = useQuery({
     queryKey: ["movie", query, currentPage],
     queryFn: () => movieService(query, currentPage),
     enabled: query !== "",
     placeholderData: keepPreviousData,
   });
-
-  const totalPages = data?.total_pages ?? 0;
 
   useEffect(() => {
     if (data && data.results.length === 0) {
@@ -42,9 +42,24 @@ export default function App() {
     setIsModalOpen(true);
   };
 
+  const totalPages = data?.total_pages ?? 0;
+
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
+      {isSuccess && totalPages > 1 && (
+        <ReactPaginate
+          pageCount={totalPages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setCuurentPage(selected + 1)}
+          forcePage={currentPage - 1}
+          containerClassName={css.pagination}
+          activeClassName={css.active}
+          nextLabel="→"
+          previousLabel="←"
+        />
+      )}
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
       {data && data.results.length > 0 && (
